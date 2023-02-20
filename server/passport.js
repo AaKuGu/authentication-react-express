81f9d685deca7a3354b068b68006624c4746d69c
AaKuGu <prathamtogupta11@gmail.com>
2023-02-20 23:50:31 +0530

    check

---
+++ server/passport.js
@@ -0,0 +1,16 @@
+import passport from "passport";
+
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      done(null, profile);
+    }
+  )
+);
