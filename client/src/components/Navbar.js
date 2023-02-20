bcf56aa96027603ab98d3822efdfa14df5c15a31
AaKuGu <prathamtogupta11@gmail.com>
2023-02-20 21:53:57 +0530

    first commit

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <div className='w-full h-[70px] bg-purple-800'>
+        {/* <Link to="" */}
+    </div>
+  )
+}
+
+export default Navbar
---
+++ server/index.js
@@ -0,0 +1,12 @@
+import express from "express";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+
+const app = express();
+
+// const PORT = process.env.PORT
+
+app.get("/", (req, res) => {
+  res.status(200).send({ message: "api is working...!" });
+});
