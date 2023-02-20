ab4709da7eb9e9c1c5db647c02d04acfb0ca5ede
AaKuGu <prathamtogupta11@gmail.com>
2023-02-21 00:31:11 +0530

    will this work

--- server/index.js
+++ server/index.js
@@ -2,12 +2,14 @@ import express from "express";
 // import dotenv from "dotenv";
 import passport from "passport";
 import { router } from "./router/authRouter";
-
-
+import cors from "cors";
 
 // dotenv.config();
 
 const app = express();
+
+app.use(cors());
+app.use(express.json());
 
 app.use(passport.initialize());
 
@@ -17,6 +19,6 @@ app.get("/", (req, res) => {
   res.status(200).send({ message: "api is working...!" });
 });
 
-app.use("/auth", router)
+app.use("/auth", router);
 
 app.listen(process.env.PORT);
--- server/router/authRouter.js
+++ server/router/authRouter.js
@@ -1,12 +1,36 @@
 import { Router } from "express";
+import passport from "passport";
 
 export const router = Router();
 
-router.get("/google", (req, res) => {
-  const name = "Aadarsh";
-  res.status(200).send({ name: name });
+router.get("/login/failed", (req, res) => {
+  res.status(400).json({
+    success: false,
+    message: "failure",
+  });
 });
 
-router.get("/sohan", (req, res) => {
-  res.status(200).send("sohan is working fine... !");
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+router.get(
+  "/google/callback",
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_API,
+    failureRedirect: "/login/failed",
+  })
+);
+
+router.get("/login/success", (req, res) => {
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req.user,
+    });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.logout();
+  res.redirect(process.env.CLIENT_API);
 });
