52c033bd5bbb8f67b9d78e76955edae23610f1a1
AaKuGu <prathamtogupta11@gmail.com>
2023-02-21 00:09:28 +0530

    sohan

--- server/router/authRouter.js
+++ server/router/authRouter.js
@@ -6,3 +6,7 @@ router.get("/google", (req, res) => {
   const name = "Aadarsh";
   res.send(200).send({ name: name });
 });
+
+router.get("/sohan", (req, res) => {
+  res.send(200).send("sohan is working fine... !");
+});
