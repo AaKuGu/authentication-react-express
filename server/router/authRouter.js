abc666e34e32b2beaba3b77ee13b41c046e56ba3
AaKuGu <prathamtogupta11@gmail.com>
2023-02-20 23:57:23 +0530

    json

--- server/router/authRouter.js
+++ server/router/authRouter.js
@@ -3,5 +3,5 @@ import { Router } from "express";
 export const router = Router();
 
 router.get("/google", (req, res) => {
-  res.send(200).send({ message: "/auth/google is working fine" });
+  res.send(200).json({ message: "/auth/google is working fine" });
 });
