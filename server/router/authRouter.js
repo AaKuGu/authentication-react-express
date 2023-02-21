a31b999708bcf17043e954565a43112ce8b3a1ab
AaKuGu <prathamtogupta11@gmail.com>
2023-02-21 13:29:04 +0530

    hallo

--- server/router/authRouter.js
+++ server/router/authRouter.js
@@ -2,54 +2,32 @@ const router = require("express").Router();
 const passport = require("passport");
 
 router.get("/login/success", (req, res) => {
-  try {
-    if (req.user) {
-      res.status(200).json({
-        success: true,
-        message: "successfull",
-        user: req.user,
-      });
-    }
-  } catch (error) {
-    res.status(400).send("error /login/success : ", error.message);
+  if (req.user) {
+    res.status(200).json({
+      success: true,
+      message: "successfull",
+      user: req.user,
+    });
   }
 });
 
 router.get("/login/failed", (req, res) => {
-  try {
-    res.status(400).json({
-      success: false,
-      message: "failure",
-    });
-  } catch (error) {
-    res.status(400).send("error /login/success : ", error.message);
-  }
+  res.status(400).json({
+    success: false,
+    message: "failure",
+  });
 });
 
-router.get("/google", (req, res) => {
-  try {
-    passport.authenticate("google", { scope: ["profile"] });
-  } catch (error) {
-    res.status(400).send("error /login/success : ", error.message);
-  }
-});
+router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
 router.get("/google/callback", (req, res) => {
-  try {
-    passport.authenticate("google", {
-      successRedirect: process.env.CLIENT_API,
-      failureRedirect: "/login/failed",
-    });
-  } catch (error) {
-    res.status(400).send("error /login/success : ", error.message);
-  }
+  passport.authenticate("google", {
+    successRedirect: process.env.CLIENT_API,
+    failureRedirect: "/login/failed",
+  });
 });
 
 router.get("/logout", (req, res) => {
-  try {
-    req.logout();
-    res.redirect(process.env.CLIENT_API);
-  } catch (error) {
-    res.status(400).send("error /login/success : ", error.message);
-  }
+  req.logout();
+  res.redirect(process.env.CLIENT_API);
 });
